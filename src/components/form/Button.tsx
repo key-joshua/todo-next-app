d46da616be714d2d9f29fb17ebc9df26257a3336
Josue Byiringiro <josue.byiringiro@skrey-software.com>
2024-03-26 22:50:52 +0200

    redirect on the server side

--- src/components/form/Button.tsx
+++ src/components/form/Button.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import { ButtonType } from '@/types/Types';
+import { Circles } from 'react-loader-spinner';
+
 
 
 const Button = (props: ButtonType) => {
     return (
         <button
             type={props?.type}
-            disabled= {props?.disabled}
+            disabled={props?.disabled}
             className='w-full text-white bg-blue-600 hover:bg-blue-600 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-600 dark:focus:ring-primary-800'>
-            {props?.fieldname}
+            { props?.disabled ? <Circles  width='30' height='30' visible={true} color='#ffffff' wrapperStyle={{ justifyContent: 'center', color: '#ffffff' }} /> : props?.fieldname }
         </button>
 
     )
