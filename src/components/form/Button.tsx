e5f77b369ab131f667c70f13fd705528cc5495f9
Josue Byiringiro <josue.byiringiro@skrey-software.com>
2024-03-26 22:51:34 +0200

    added load spinner

--- src/components/form/Button.tsx
+++ src/components/form/Button.tsx
@@ -10,7 +10,7 @@ const Button = (props: ButtonType) => {
             type={props?.type}
             disabled={props?.disabled}
             className='w-full text-white bg-blue-600 hover:bg-blue-600 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-600 dark:focus:ring-primary-800'>
-            { props?.disabled ? <Circles  width='30' height='30' visible={true} color='#ffffff' wrapperStyle={{ justifyContent: 'center', color: '#ffffff' }} /> : props?.fieldname }
+            {props?.disabled ? <Circles width='30' height='30' visible={true} color='#ffffff' wrapperStyle={{ justifyContent: 'center', color: '#ffffff' }} /> : props?.fieldname}
         </button>
 
     )
